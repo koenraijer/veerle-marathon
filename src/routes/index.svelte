dedb4e290a69b0bac0b9247bb6bf8b6bae3633f7
Koen Raijer <koenraijer@gmail.com>
2022-02-18 14:55:17 +0100

    layout changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
-<div class="grid grid-cols-1 md:grid-cols-2 gap-x-12">
-    <div class="md:col-span-2 z-10">
-        <h1 class="text-6xl md:text-8xl font-bold z-10">Marathon Rotterdam</h1>
-        <h2 class="text-2xl my-4">Kanker bestrijden is topsport!</h2> 
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-x-12">
+    <div class="lg:col-span-2 z-10">
+        <h1 class="text-4xl sm:text-6xl md:text-8xl font-bold z-10">Marathon Rotterdam</h1>
+        <h2 class="text-xl md:ml-4 my-4">Kanker bestrijden is topsport!</h2> 
     </div>
 
-    <div class="prose md:ml-4 prose-md md:prose-lg py-8 max-w-[70ch]">
+    <div class="prose lg:ml-4 prose-md md:prose-lg py-8 max-w-[70ch]">
         <p>
             10 April a.s. ren ik (Veerle Ravensberg) de NN Marathon in Rotterdam. Bij deze marathon staat de kankerbestrijding centraal.
         </p>
@@ -18,28 +18,32 @@
         </p>
         <p>
             Help mij de Toon Hermans Huizen te sponsoren opdat zij hun mooie werk voort kunnen zetten en veel zieke mensen een fijne dag kunnen geven.
-            Stort een bijdrage op ..x.xx….x.x.x.x.x.x.x.x.x voor een knipoog van Toon.
         </p>
     </div>
 
-    <div class="grid grid-cols-2 md:-mt-28">
-        <div class="col-span-2">
-            <img class="md:mx-6 w-full h-auto aspect-square -z-10" alt="Veerle en Wilco met Toon Hermans" src="hero.jpeg">
+    <div class="grid grid-cols-2 lg:-mt-28 pb-48">
+        <div class="relative col-span-2">
+            <img class="md:mx-6 mx-auto md:h-auto md:max-h-[55vh] aspect-square -z-10" alt="Veerle en Wilco met Toon Hermans" src="hero.jpeg">
+            <img class="absolute aspect-square right-6 -translate-y-12 w-auto h-56 " alt="Logo Toon Hermans"src="toonhermans.jpeg">
         </div>
-        <div class="relative">
-            <img class="transform aspect-square translate-x-12 -translate-y-16 max-h-56 " alt="Logo Toon Hermans"src="toonhermans.jpeg">
-        </div>
-        <div class="relative my-8 col-span-2 mx-auto">
-            <blockquote class="prose italic prose-xl">
-                    Vandaag is de dag<br>
-                    hij komt maar één keer,<br>
-                    morgen dan is het<br>
-                    vandaag al niet meer.<br>
-                    Niet zeuren, geniet <br>
-                    van het leven, het mag,<br>
-                    maar doe het vandaag<br>
-                    want vandaag is de dag. <br>
-            </blockquote>
-        </div>
+    </div>
+</div>
+<div class="grid my-4 grid-cols-1 lg:grid-cols-2 gap-8">
+    <div class="relative mx-auto lg:col-start-2">
+        <blockquote class="prose mb-8 italic prose-2xl">
+                Vandaag is de dag<br>
+                hij komt maar één keer,<br>
+                morgen dan is het<br>
+                vandaag al niet meer.<br>
+                Niet zeuren, geniet <br>
+                van het leven, het mag,<br>
+                maar doe het vandaag<br>
+                want vandaag is de dag. <br>
+        </blockquote>
+    </div>
+    <div class="prose place-self-center prose-lg lg:col-start-1 lg:row-start-1 ml-4 lg:px-12 font-semibold">
+        <h3>
+            Stort een bijdrage op ..x.xx….x.x.x.x.x.x.x.x.x voor een knipoog van Toon.
+        </h3>
     </div>
 </div>
