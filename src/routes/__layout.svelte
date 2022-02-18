3f7b94a53816237b284a491f5f389291157d45f6
Koen Raijer <koenraijer@gmail.com>
2022-02-18 15:01:51 +0100

    SEO added

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,9 @@
 </script>
 
 <svelte:head>
+	<title>Marathon Rotterdam</title>
+	<meta name="description" content="Veerle Ravensberg NN Marathon Rotterdam voor Toon Hermans Huizen.">
+
 	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="faviconfavicon-16x16.png">
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,13 +24,13 @@
     <div class="grid grid-cols-2 lg:-mt-28 pb-48">
         <div class="relative col-span-2">
             <img class="md:mx-6 mx-auto md:h-auto md:max-h-[55vh] aspect-square -z-10" alt="Veerle en Wilco met Toon Hermans" src="hero.jpeg">
-            <img class="absolute aspect-square right-6 -translate-y-12 w-auto h-56 " alt="Logo Toon Hermans"src="toonhermans.jpeg">
+            <img class="absolute aspect-square right-6 -translate-y-12 w-auto max-h-[40vw]" alt="Logo Toon Hermans"src="toonhermans.jpeg">
         </div>
     </div>
 </div>
 <div class="grid my-4 grid-cols-1 lg:grid-cols-2 gap-8">
     <div class="relative mx-auto lg:col-start-2">
-        <blockquote class="prose mb-8 italic prose-2xl">
+        <blockquote class="prose mb-8 italic prose-lg lg:prose-2xl">
                 Vandaag is de dag<br>
                 hij komt maar één keer,<br>
                 morgen dan is het<br>
