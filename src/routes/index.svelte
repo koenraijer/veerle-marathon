2e1f083c947278be6a379a7cb9af32b5e349ff63
Koen Raijer <koenraijer@gmail.com>
2022-02-21 07:22:52 +0100

    final changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,60 @@
 <div class="grid grid-cols-1 lg:grid-cols-2 gap-x-12">
     <div class="lg:col-span-2 z-10">
         <h1 class="text-4xl sm:text-6xl md:text-8xl font-bold z-10">Marathon Rotterdam</h1>
-        <h2 class="text-xl md:ml-4 my-4">Kanker bestrijden is topsport!</h2> 
+        <h2 class="text-lg md:ml-4 my-4">Zondag 10 April 2022</h2> 
     </div>
 
     <div class="prose lg:ml-4 prose-md md:prose-lg py-8 max-w-[70ch]">
+        <h3 class="text-xl">Kanker bestrijden doe je samen!</h3>
         <p>
-            10 April a.s. ren ik (Veerle Ravensberg) de NN Marathon in Rotterdam. Bij deze marathon staat de kankerbestrijding centraal.
+            Hardlopen. Zit het in de genen? Een aantal jaren geleden 
+            ben ik in de leer gegaan bij de beste meester, mijn vader. 
+            Al 40 jaar hardloper in hart en nieren. 
         </p>
         <p>
-            Mijn vader kreeg vorig jaar volledig onverwacht te maken met een grote tumor. Na een periode van ziekenhuizen, operaties en ellende volgt nu een periode van herstel en strijd tegen het voorkomen van terugkeer.  
+            <b>September ’20</b>. samen mijn eerste Trail in Valkenburg gelopen.
+            Bucketlist wedstrijden besproken. 
         </p>
         <p>
-            Kanker bestrijden is topsport. Je hebt uithoudingsvermogen nodig en een vrolijke kijk op het leven. Vaak gaat het goed, maar soms ook niet. 
-            Dan zijn de Toon Hermans Huizen er.  Daar kun je met lotgenoten praten en activiteiten doen op een fijne manier. Alles begeleid door vrijwilligers en in de geest van Toon. Met een lach en met een traan. Het is een grote hulp om verder te komen in moeilijke tijden.
-            Kanker bestrijd je samen. Daarom vraag ik ook om jouw hulp. 
+            <b>Februari ’21</b>. Kanker gooit roet in het eten. 
         </p>
         <p>
-            Help mij de Toon Hermans Huizen te sponsoren opdat zij hun mooie werk voort kunnen zetten en veel zieke mensen een fijne dag kunnen geven.
+            Kanker bestrijden is topsport en confronterend. 
+            Niet alleen als je zelf ziek bent, maar ook als iemand in je 
+            directe omgeving is geconfronteerd. 
+        </p>
+        <p>
+            Dan zijn er de Toon Hermans huizen. Een open huis voor
+            mensen met kanker, hun naasten en nabestaanden. 
+            Hier kun je met lotgenoten praten, een kopje koffie drinken,
+            meedoen aan diverse activiteiten. Alles begeleid door 
+            vrijwilligers. 
+        </p>
+        <p>
+            <b>Juni’21</b>. Hij loopt weer een minuutje mee. 
+            Om het weer even samen te doen.         
+        </p>
+        <p>
+            Veel trainingen volgen die nauw op de voet worden 
+            gevolgd door mijn vader. Elke week weer nieuwe 
+            tips & tricks. 
+        </p>
+        <p>
+            <b>April ’22</b>. Marathon Rotterdam. Volledig in het teken van 
+            Kanker. Ik loop de Marathon voor het Toon Hermans huis!
+        </p>
+        <p>
+            Help mij de Toon Hermans Huizen te sponsoren zodat zij
+            hun mooie werk kunnen voortzetten en veel zieke mensen,
+            hun naasten en nabestaanden, een fijne dag kunnen
+            geven. 
         </p>
     </div>
 
     <div class="grid grid-cols-2 lg:-mt-28 pb-48">
         <div class="relative col-span-2">
             <img class="md:mx-6 mx-auto md:h-auto md:max-h-[55vh] aspect-square -z-10" alt="Veerle en Wilco met Toon Hermans" src="hero.jpeg">
-            <img class="absolute aspect-square right-6 -translate-y-16 w-auto max-h-[40vw] md:max-h-56" alt="Logo Toon Hermans"src="toonhermans.jpeg">
+            <img class="absolute aspect-square right-16 -translate-y-12 w-auto max-h-[40vw] md:max-h-64" alt="Logo Toon Hermans"src="toonhermans.png">
         </div>
     </div>
 </div>
@@ -42,8 +72,12 @@
         </blockquote>
     </div>
     <div class="prose place-self-center prose-lg lg:col-start-1 lg:row-start-1 ml-4 lg:px-12 font-semibold">
-        <h3>
-            Stort een bijdrage op ..x.xx….x.x.x.x.x.x.x.x.x voor een knipoog van Toon.
+        <a href="https://tikkie.me/pay/kb3qghhqfga712nvjd10" rel="noopener noreferrer" target="_blank">
+            <img class="mx-auto" src="tikkie.png" alt="Tikkie">
+        </a>
+        <h3 class="text-center">
+            Stort een bijdrage op NL97 ABNA 0108.5490.89 T.n.v. Veerle Ravensberg 
+            voor een knipoog van Toon. 
         </h3>
     </div>
 </div>
